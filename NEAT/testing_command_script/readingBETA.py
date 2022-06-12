b6b4110eae7a2a59383b3266441df0767aa9e2da
Varenya Jain <78588847+VarenyaJ@users.noreply.github.com>
2022-06-12 16:57:37 -0500

    Pushing latest files

--- NEAT/testing_command_script/readingBETA.py
+++ NEAT/testing_command_script/readingBETA.py
@@ -1,58 +1,52 @@
 import sys
 sys.path.append("/home/suvinit/NEAT/testing_command_script/test_command")
-import test_command as tc_file
 #
 def HelloThereGeneralKenobi():
     try:
-        with open('config.txt', 'r') as f:
+        with open('config.txt', 'rt') as FILEREAD:
             print("Does this work? #1\n")
             print("\nreadlines makes:\n")
-            print(f.readlines())
-            f.seek(0)
+            print(FILEREAD.readlines())
+            FILEREAD.seek(0)
             print("\nCheck 1\n")
             #
-            print("\nThis is where the fun begins\n")
-            Text = ["0"]
-            Text_Cur = ""
-            for i in range (len(f.readlines())):
-                print("length of text inside the document is " + f"{len(f.readlines())}" )
-                Text[i] = f.readlines()[i]
-                Text_Cur = Text[Text.find("@")+1:].split()
-                print("\n" + Text_Cur + "\n")
-            print("Bah-weep-Graaaaagnah wheep ni ni bong")
+            print("\nThis is where the fun begins:\n")
+            read_vars = FILEREAD.read()
+            print("\n" + f"{read_vars}")
             #
-            print("read makes:\n")
-            f.seek(0)
-            print(f.read())
-            f.seek(0)
-            print("\nPost 2\n")
-            #
-        #
-        '''
-        with open('config.txt', 'r') as f:import /home/suvinit/NEAT/testing_command_script/test_command
-            f.seek(0)
-            print("Does this work? #1\n")
-            print("The config file contains the following:\n")
-            #to print out the array of all lines with brackets, use:print(f.readline())
-            #to print out every line print(f.readline())
-            print(f.read())
-            #print()
-            #f.seek(0)
-            ###
-            r = tc_file.file_path
-            R = tc_file.read_length
-            o = tc_file.output_path
-            print('the value of r is %s\n' % str(r))
-            print('the value of R is %s\n' % int(R))
-            print('the value of o is %s\n' % str(o))
-            print('the value of bam is %s\n' % bool(tc_file.bam))
             '''
+            print("\nCheck 2\n")
+            read_line_by_line = FILEREAD.readlines()
+            print("\nreading lines:")
+            print("\nCheck 3\n")
+            for read_line_by_line in FILEREAD:
+                print("\n" + f"{read_line_by_line}")
+            print("\nCheck 4\n")
+            '''
+            ####    ASK JOSH FOR HELP WITH THIS PART    ####
+        #Strip Newlines
+        mylines = []
+        with open ('config.txt', "rt") as myfile:
+            for line in myfile:
+                mylines.append(line)
+            print("\nCheck 4.1\n")
+            for element in mylines:
+                print(element)
+            print("\nCheck 5\n")
+            myfile.seek(0)
+            for line in myfile:
+                print(mylines[0].find" = ")
+        print("\nCheck 6\n")
+                #
             #
-        #
-
     except FileNotFoundError:
             print("There is a FileNotFoundError\n")
-    print("\nHi Josh!")
-
+            print("\nHi Josh!")
+            #
+        #
+    #
+#
 if __name__ == '__main__':
-	HelloThereGeneralKenobi()
+    print("Check 0\n")
+    HelloThereGeneralKenobi()
+    #print("Bah-weep-Graaaaagnah wheep ni ni bong")
--- NEAT/testing_command_script/test_command.py
+++ NEAT/testing_command_script/test_command.py
@@ -1,36 +1,19 @@
 import click
 import os.path
-
-# no caps
-
 @click.command()
-@click.option('--file_path', required=True, show_default=True, help='Path to reference fasta', type=click.Path(exists=True))
+#@click.option('--file_path', required=True, show_default=True, help='Path to reference fasta', type=click.Path(exists=True))
+@click.option('--file_path', required=True, show_default=True, default = "/home/suvinit/NEAT-data/H1N1/H1N1.fa", help='Path to reference fasta', type=click.Path(exists=True))
+#find a fix for capitalization
 @click.option('--read_length', required=True, default=100, show_default=True, help='The desired read length') #try to test is < this is within a certain range (50,300)
 @click.option('--output_path', default="/home/suvinit/NEAT/testing_command_script/H1N1.test-run", show_default=True, help='output_prefix')
 @click.option('--bam', default=False, is_flag=True, show_default=True, help='output golden BAM file')
-###
-#@click.option('--n', default=1, show_default=True)
-#@click.option("--gr", is_flag=True, show_default=True, default=False, help="Greet the world.")
-#@click.option("--br", is_flag=True, show_default=True, default=True, help="Add a thematic break")
-################################<TASK BELOW>################################
 #try validating that the file directory exists (/home/suvinit)
-def hello(file_path, read_length, output_path, bam):
-
+def gen_config_file(file_path, read_length, output_path, bam):
 	click.echo(f'\nHello, the input path the a reference fasta is: {file_path}' + f'\nHello, the read length is: {read_length}' + f'\nHello, the output path is: {output_path}')
 	click.echo("\nCongrats Varenya! This code actually compiles!")
 	click.echo("(with a --file_path followed by an actual filepath, like /home/suvinit/NEAT-data/H1N1/H1N1.fa)")
-
-#	click.echo(f'\noutput bam test line:\n {bam}')
-	###
-#	if gr:
-#		click.echo('Hello world!')
-#		click.echo('\n')
-#		click.echo('.' * n)
-#	if br:
-#		click.echo('\n')
-#		click.echo('-' * n)
 	try:
-		with open('config.txt', 'w') as f:
+		with open('neat.cfg', 'w') as f:
 			f.write("Created/overwrote a config file")
 			r = file_path
 			R = read_length
@@ -43,11 +26,33 @@ def hello(file_path, read_length, output_path, bam):
 	except FileNotFoundError:
 			print("The directory does not exist")
 	print("\nHi Josh!")
+	print("<hello> is done")
 
+#combine code from readingBETA
+def read_back_cfg():
+	print("Check 2.1")
+	try:
+		mylines = []
+		with open ('neat.cfg', "rt") as myfile:
+			for line in myfile:
+				mylines.append(line)
+			print("\nCheck 2.2\n")
+			for element in mylines:
+				print(element)
+			print("\nCheck 2.3\n")
+			myfile.seek(0)
+			for line in myfile:
+				print(mylines[0].find(" = "))
+			print("\nCheck 2.4\n")
+	except FileNotFoundError:
+            print("There is a FileNotFoundError\n")
+            print("\nHi Josh!")
 
 if __name__ == '__main__':
-	hello()
-################################<TASK BELOW>################################
+	gen_config_file()
+	print("\nBah-weep-Graaaaagnah weep ni ni bong\n")
+	read_back_cfg()
+################################<THOUGHTS BELOW>################################
 #When opening config.txt in write-mode
 #Realized that the config file only opens in same directory as script
 #Maybe change that?
