51d441b42f2459f4aa3c7e5e78a2f7aa4bd40211
Varenya Jain <78588847+VarenyaJ@users.noreply.github.com>
2022-07-26 10:40:13 -0500

    07/21 work

--- neat/cli/commands/read_simulator_cli.py
+++ neat/cli/commands/read_simulator_cli.py
@@ -8,6 +8,8 @@ from ...read_simulator import read_simulator_runner
 from .base import BaseCommand
 from .options import output_group
 import yaml
+from collections import OrderedDict
+import os
 
 
 class Command(BaseCommand):
@@ -29,21 +31,21 @@ class Command(BaseCommand):
 
         :param parser: The parser to add arguments to.
         """
-        parser.add_argument('-r', type=str, dest = 'Reference File', required=True, metavar='reference', help="Path to reference fasta")
-        parser.add_argument('-R', type=int, dest = 'Read Length', required=True, metavar='read_length', help="The desired read length (ideally between 100 and 300)")
-        parser.add_argument('--bam', dest = 'Bam Output', required=False, action='store_true', default=False, help="output golden BAM file")
-        parser.add_argument('-c', type=float, dest = 'Average Coverage', required=False, metavar='coverage', default=10.0, help="Average coverage, default is 10.0")
-        parser.add_argument('-e', type=str, dest = 'Error Model', required=False, metavar='error_model', default=None, help="Location of the file for the sequencing error model (omit to use the default)")
+        parser.add_argument('-r', type=str, dest = 'reference', required=True, metavar='reference', help="Path to reference fasta")
+        parser.add_argument('-R', type=int, dest = 'read_len', required=True, metavar='read_length', help="The desired read length (ideally between 100 and 300)")
+        parser.add_argument('--bam', dest = 'produce_bam', required=False, action='store_true', default=False, help="output golden BAM file")
+        parser.add_argument('-c', type=float, dest = 'coverage', required=False, metavar='coverage', default=10.0, help="Average coverage, default is 10.0")
+        parser.add_argument('-e', type=str, dest = 'error_model', required=False, metavar='error_model', default=None, help="Location of the file for the sequencing error model (omit to use the default)")
         parser.add_argument('-E', type=float, dest = 'Error Rate', required=False, metavar='Error rate', default=-1, help="Rescale avg sequencing error rate to this, must be between 0.0 and 0.3")
-        parser.add_argument('-p', type=int, dest = 'Desired Ploidy', required=False, metavar='ploidy', default=2, help="Desired ploidy, default = 2")
-        parser.add_argument('-tr', type=str, dest = 'Target Region File', required=False, metavar='target.bed', default=None, help="Bed file containing targeted regions")
-        parser.add_argument('-dr', type=str, dest = 'Discard Region File', required=False, metavar='discard_regions.bed', default=None, help="Bed file with regions to discard")
-        parser.add_argument('-to', type=float, dest = 'Off Target Coverage Scalar', required=False, metavar='off-target coverage scalar', default=0.00, help="off-target coverage scalar")
-        parser.add_argument('-m', type=str, dest = 'Mutation Model File', required=False, metavar='model.p', default=None, help="Mutation model pickle file")
-        parser.add_argument('-M', type=float, dest = 'Mutation Rescale Rate', required=False, metavar='avg mut rate', default=-1, help="Rescale avg mutation rate to this (1/bp), must be between 0 and 0.3")
-        parser.add_argument('-Mb', type=str, dest = 'Positional Mutation File', required=False, metavar='mut_rates.bed', default=None, help="Bed file containing positional mut rates")
-        parser.add_argument('-N', type=int, dest = 'Minimum Quality Score', required=False, metavar='min qual score', default=-1, help="below this quality score, replace base-calls with N's")
-        parser.add_argument('-v', type=str, dest = 'Variant Input', required=False, metavar='vcf.file', default=None, help="Input VCF file of variants to include")
+        parser.add_argument('-p', type=int, dest = 'ploidy', required=False, metavar='ploidy', default=2, help="Desired ploidy, default = 2")
+        parser.add_argument('-tr', type=str, dest = 'target_bed', required=False, metavar='target.bed', default=None, help="Bed file containing targeted regions")
+        parser.add_argument('-dr', type=str, dest = 'discard_bed', required=False, metavar='discard_regions.bed', default=None, help="Bed file with regions to discard")
+        parser.add_argument('-to', type=float, dest = 'off_target_scalar', required=False, metavar='off-target coverage scalar', default=0.00, help="off-target coverage scalar")
+        parser.add_argument('-m', type=str, dest = 'mutation_model', required=False, metavar='model.p', default=None, help="Mutation model pickle file")
+        parser.add_argument('-M', type=float, dest = 'discard_bed', required=False, metavar='avg mut rate', default=-1, help="Rescale avg mutation rate to this (1/bp), must be between 0 and 0.3")
+        parser.add_argument('-Mb', type=str, dest = 'mutation_rate', required=False, metavar='mut_rates.bed', default=None, help="Bed file containing positional mut rates")
+        parser.add_argument('-N', type=int, dest = 'min_mutations', required=False, metavar='min qual score', default=-1, help="below this quality score, replace base-calls with N's")
+        parser.add_argument('-v', type=str, dest = 'include_vcf', required=False, metavar='vcf.file', default=None, help="Input VCF file of variants to include")
 
         parser.add_argument('--pe', nargs=2, type=int, dest = 'Paired End Values', required=False, metavar=('<int>', '<int>'), default=(None, None), help='Paired-end fragment length mean and std')
         parser.add_argument('--pe-model', type=str, required=False, metavar='<str>', default=None, help='empirical fragment length distribution')
@@ -70,10 +72,22 @@ class Command(BaseCommand):
         my_dict = {}
         for key in args_dict.keys():
             my_dict[key] = args_dict[key]
-        #
+        #07/21/2022 task
+        #Try altering the for loop to only pass through variable we rng_seed
+        #i.e. a specific set of variables (maybe the basic 4?)
+        #Possibly try loading the simple yaml template as a dict then filling it out
+        #Ask Josh to repeat and clarify the ploting task
 
         with open('neat.cfg', 'w') as file:
             documents = yaml.dump(my_dict, file)
+
+        #Try storing Python dictionary entries in the order they are pushed
+        test_dict = OrderedDict()
+        for key in args_dict.keys():
+            test_dict[key] = args_dict[key]
+        with open('2neat.cfg', 'w') as file:
+            documents = yaml.dump(test_dict, file)
+        #
 
 
         #read_simulator_runner(arguments.config, arguments.output)
--- neat/read_simulator/runner.py
+++ neat/read_simulator/runner.py
@@ -1,5 +1,5 @@
 """
-Runner for read_simulator task
+Runner for generate_reads task
 """
 
 import logging
@@ -9,8 +9,12 @@ import gzip
 from Bio import SeqIO
 from pathlib import Path
 
+from .utils import Options, parse_input_vcf, parse_bed, OutputFileWriter, find_file_breaks, map_chromosome, \
+    generate_variants
 from ..common import validate_input_path, validate_output_path
-from .utils import Options, parse_input_vcf, parse_bed
+from ..models import MutationModel, SequencingErrorModel, FragmentLengthModel, GcModel
+from ..models.default_cancer_mutation_model import *
+
 
 __all__ = ["read_simulator_runner"]
 
@@ -26,29 +30,62 @@ def initalize_all_models(options: Options):
     :param options: the options for this run
     """
 
-    mut_model = pickle.load(gzip.open(options.error_model, "rb"))
+    if options.mutation_model:
+        mut_model = pickle.load(gzip.open(options.mutation_model))
+    else:
+        mut_model = MutationModel()
+    # Set the rng for the mutation model
     mut_model.rng = options.rng
+    # Set user input mutation rate:
+    if options.mutation_rate:
+        mut_model.avg_mut_rate = options.mutation_rate
 
     cancer_model = None
-    if options.cancer:
-        cancer_model = pickle.load(gzip.open(options.cancer_model, 'rb'))
+    if options.cancer and options.cancer_model:
+        cancer_model = pickle.load(gzip.open(options.cancer_model))
+        # Set the rng for the cancer mutation model
         cancer_model.rng = options.rng
+    elif options.cancer:
+        cancer_model = MutationModel(avg_mut_rate=default_cancer_avg_mut_rate,
+                                     homozygous_freq=default_cancer_homozygous_freq,
+                                     insertion_chance=default_cancer_insertion_chance,
+                                     deletion_chance=default_cancer_deletion_chance,
+                                     trinuc_trans_matrices=default_cancer_trinuc_trans_matrices,
+                                     trinuc_trans_bias=default_cancer_trinuc_trans_bias,
+                                     insertion_lengths=default_cancer_insertion_lengths,
+                                     insertion_weights=default_cancer_insertion_weights,
+                                     deletion_lengths=default_cancer_deletion_lengths,
+                                     deletion_weights=default_cancer_deletion_weights,
+                                     is_cancer=True,
+                                     rng=options.rng)
 
     _LOG.debug("Mutation models loaded", 'debug')
 
     # We need sequencing errors to get the quality score attributes, even for the vcf
-    error_model = pickle.load(gzip.open(options.error_model, 'rb'))
+    if options.error_model:
+        error_model = pickle.load(gzip.open(options.error_model))
+    else:
+        error_model = SequencingErrorModel()
+    # Set the rng for the sequencing error model
     error_model.rng = options.rng
 
     _LOG.debug('Sequencing error model loaded', 'debug')
 
     # initialize gc_model
-    gc_model = pickle.load(gzip.open(options.gc_model))
+    if options.gc_model:
+        gc_model = pickle.load(gzip.open(options.gc_model))
+    else:
+        gc_model = GcModel()
+    # Set the rng for the GC bias model
     gc_model.rng = options.rng
 
     _LOG.debug('GC Bias model loaded', 'debug')
 
-    fraglen_model = pickle.load(gzip.open(options.fragment_model))
+    if options.fragment_model:
+        fraglen_model = pickle.load(gzip.open(options.fragment_model))
+    else:
+        fraglen_model = FragmentLengthModel()
+    # Set the rng for the fragment length model
     fraglen_model.rng = options.rng
 
     _LOG.debug("Fragment length model loaded", 'debug')
@@ -78,12 +115,23 @@ def read_simulator_runner(config: str, output: str):
     _LOG.debug(f'config = {config}')
     _LOG.debug(f'output = {output}')
 
-    validate_input_path(config)
     _LOG.info(f'Using configuration file {config}')
-    validate_output_path(output, False)
-    _LOG.info(f"Saving output files to {Path(output).parent}")
+    config = Path(config).resolve()
+    validate_input_path(config)
 
-    options = Options(Path(output), config)
+    # prepare output
+    _LOG.info(f"Saving output files to {Path(output).parent}")
+    output = Path(output).resolve()
+
+    if not output.parent.is_dir():
+        _LOG.info('Creating output dir')
+        output.parent.mkdir(parents=True, exist_ok=True)
+
+    # Read options file
+    options = Options(output, config)
+
+    # Validate output
+    validate_output_path(output, False, options.overwrite_output)
 
     """
     Model preparation
@@ -103,13 +151,13 @@ def read_simulator_runner(config: str, output: str):
     """
     Process Inputs
     """
-    _LOG.info("Processing inputs.")
     _LOG.info(f'Reading {options.reference}.')
 
-    reference_index = SeqIO.index(str(options.reference), 'Fasta')
+    reference_index = SeqIO.index(str(options.reference), 'fasta')
     _LOG.debug("Reference file indexed.")
 
     reference_contigs = list(reference_index.keys())
+    options.set_value("reference_contigs", reference_contigs)
 
     input_variants = None
     if options.include_vcf:
@@ -137,12 +185,104 @@ def read_simulator_runner(config: str, output: str):
     if options.target_bed or options.discard_bed or options.mutation_bed:
         _LOG.info(f"Reading input bed files.")
 
-    # Note that parse_bed will return None for any empty or missing files
-    target_regions_dict = parse_bed(options.target_bed, options.reference_chromosomes,
+    # Note that parse_bed will return None for any empty or non-input files
+    target_regions_dict = parse_bed(options.target_bed, options.reference_contigs,
                                     False)
 
-    discard_regions_dict = parse_bed(options.discard_bed, options.reference_chromosomes,
+    discard_regions_dict = parse_bed(options.discard_bed, options.reference_contigs,
                                      False)
 
-    mutation_rate_dict = parse_bed(options.mutation_bed, options.reference_chromosomes,
+    mutation_rate_dict = parse_bed(options.mutation_bed, options.reference_contigs,
                                    True)
+
+    if any([target_regions_dict, discard_regions_dict, mutation_rate_dict]):
+        _LOG.debug("Finished reading input beds.")
+
+    # Prepare headers
+    bam_header = None
+    if options.produce_bam:
+        # This is a dictionary that is the list of the contigs and the length of each.
+        # This information will be needed later to create the bam header.
+        bam_header = {key: len(reference_index[key]) for key in reference_index.keys()}
+
+    # Creates files and sets up objects for files that can be written to as needed.
+    # Also creates headers for bam and vcf.
+    # We'll also keep track here of what files we are producing.
+    if options.cancer:
+        output_normal = options.output.parent / f'{options.output.name}_normal'
+        output_tumor = options.output.parent / f'{options.output.name}_tumor'
+        output_file_writer = OutputFileWriter(options=options,
+                                              bam_header=bam_header)
+        output_file_writer_cancer = OutputFileWriter(options=options,
+                                                     bam_header=bam_header)
+    else:
+        outfile = options.output.parent / options.output.name
+        output_file_writer = OutputFileWriter(options=options,
+                                              bam_header=bam_header)
+        output_file_writer_cancer = None
+
+    _LOG.debug(f'Output files ready for writing.')
+
+    """
+    Begin Analysis
+    """
+    _LOG.info("Beginning simulation.")
+
+    breaks = find_file_breaks(options.threads, options.partition_mode, reference_index)
+
+    _LOG.debug("Input reference partitioned for run")
+
+    # these will be the features common to each contig, for multiprocessing
+    common_features = {}
+
+    vcf_files = []
+    fasta_record = []
+    fastq_files = []
+    bam_files = []
+    print_fasta_tell = False
+
+    for contig in breaks:
+        _LOG.info(f"Generating variants for {contig}")
+
+        # Todo genericize breaks
+        local_variants = None
+        if input_variants:
+            local_variants = {x[1]: input_variants[x] for x in input_variants if x[0] == contig}
+        local_reference = reference_index[contig]
+
+        _LOG.info(f'Creating trinucleotide map for {contig}...')
+        local_trinuc_map = map_chromosome(local_reference, mut_model)
+
+        # Since we're only running single threaded for now:
+        threadidx = 1
+
+        local_variant_file = options.temp_dir_path / f'{options.output.stem}_tmp_{contig}_{threadidx}.vcf'
+        local_fasta_file = None
+        if options.produce_fasta:
+            local_fasta_file = options.temp_dir_path / f'{options.output.stem}_tmp_{contig}_{threadidx}.fasta'
+
+        _LOG.debug(f'local vcf filename = {local_variant_file}')
+
+        if threadidx == 1:
+            # init_progress_info()
+            pass
+
+        generate_variants(reference=local_reference,
+                          variant_file=local_variant_file,
+                          trinucleotide_map=local_trinuc_map,
+                          target_regions=target_regions_dict[contig],
+                          discard_regions=discard_regions_dict[contig],
+                          mutation_rate_regions=mutation_rate_dict[contig],
+                          forced_variants=local_variants,
+                          max_qual_score=max(seq_error_model.quality_scores),
+                          mutation_model=mut_model,
+                          options=options,
+                          fasta_file=local_fasta_file)
+
+        if options.produce_vcf:
+            vcf_files.append(local_variant_file)
+
+    if options.produce_vcf:
+        _LOG.info("Outputting golden vcf.")
+        output_file_writer.merge_temp_vcfs(vcf_files)
+
